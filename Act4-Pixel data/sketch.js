4b84826aa99ee98def662df5dfb63e3fda5a1ba5
harrsh206 <harshinkriss88@gmail.com>
2025-05-29 19:03:36 +0400

    Add files via upload

---
+++ Act4-Pixel data/sketch.js
@@ -0,0 +1,38 @@
+var img, x, y;
+
+function preload() {
+
+img = loadImage("M4.png");
+
+}
+
+
+function setup() {
+
+createCanvas (900, 900);
+
+background(0);
+
+noStroke();
+
+}
+
+
+function draw() {
+
+background(0);
+
+x = mouseX;
+
+y = mouseY;
+
+image( img, 0, 0);
+
+var c = get(x, y); 
+
+fill(c); 
+
+ellipse (x, y, 100, 100); 
+
+}
+
